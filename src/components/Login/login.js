4f6a79177ecf3e49610511d9b6924916a319b2b4
Navjot Singh <njot422@gmail.com>
2020-06-30 13:28:09 +0530

    login form

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -6,8 +6,10 @@ function Login() {
         <div className="content">
             <h1 className="header">Login</h1>
             <div className="login-card">
-            <input type="text" placeholder="username" className="input"></input><br />
-            <input type="password" placeholder="password" className="input"></input><br />
+            <input type="text"  className="input1"></input><br />
+            <label className="User_label">Username</label>
+            <input type="password"  className="input2"></input><br />
+            <label className="label" >Password</label>
             <button className="btn">Login</button>
             </div>
         </div>
